c1a66f17a5fd72d894afad9d133384fd319ad9fe
Tony Ayoub <tonysameh@hotmail.com>
2020-07-25 20:13:23 +0200

    Initial Commit

---
+++ RememberCards/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RememberCards
+//
+//  Created by Tony Ayoub on 7/25/20.
+//  Copyright © 2020 Tony Ayoub. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
